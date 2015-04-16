7b507d6534568f38044ab2bbee7dac7b9cd6d4eb
Remco Vlierman <remco.vlierman@ing.nl>
2015-04-16 11:27:00 +0200

    implemented barlabel logic

---
+++ src/scripts/chartist-plugin-barlabels.js
@@ -0,0 +1,41 @@
+/**
+ * Chartist.js plugin to display a data label left or right to a bar chart column.
+ *
+ */
+/* global Chartist */
+(function(window, document, Chartist) {
+  'use strict';
+
+    var defaultOptions = {
+        labelClass: 'ct-label',
+        labelOffset: {
+            x: 20,
+            y: -4
+        },
+        barAnchor: 'left'
+    };
+
+    Chartist.plugins = Chartist.plugins || {};
+    Chartist.plugins.ctBarLabels = function(options) {
+
+    options = Chartist.extend({}, defaultOptions, options);
+
+    return function ctBarLabels(chart) {
+        // bar labels
+        if(chart instanceof Chartist.Bar) {
+            chart.on('draw', function(data) {
+                var barLabelPrefix = '$ '
+                var centerPos = (data.y1 + data.y2)/2;
+                if(data.type === 'bar') {
+                    data.group.elem('text', {
+                        x: data.x1 + options.labelOffset.x,
+                        y: centerPos - options.labelOffset.y,
+                        style: 'text-anchor: ' + options.barAnchor
+                    }, options.labelClass).text(barLabelPrefix + data.value);
+                }
+            });
+        }
+    };
+  };
+
+}(window, document, Chartist));
